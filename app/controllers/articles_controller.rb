aee18b13de5e9f246648d8b01f076a2091f5ee1b
kjb085 & melissawimberly <pair+kjb085.melissawimberly@devbootcamp.com>
2015-02-24 12:09:47 -0500

    Add methods for articles and categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,38 @@
 class ArticlesController < ApplicationController
 
+  def index
+
+  end
+
+  def create
+    # Article.create(article_params)
+    # redirect_to articles_path
+    p article_params
+    redirect_to articles_path
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :description, user_id: session[:user_id], category_id: 1)
+  end
 end
